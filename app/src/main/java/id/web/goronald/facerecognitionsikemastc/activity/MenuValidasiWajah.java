4864ccdeb349681d96188708bf5baf226fed4235
SE\5113100112 <geronald10@gmail.com>
2017-03-15 20:18:32 +0700

    update activity changes

--- app/src/main/java/id/web/goronald/facerecognitionsikemastc/activity/MenuValidasiWajah.java
+++ app/src/main/java/id/web/goronald/facerecognitionsikemastc/activity/MenuValidasiWajah.java
@@ -1,15 +1,43 @@
 package id.web.goronald.facerecognitionsikemastc.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import id.web.goronald.facerecognitionsikemastc.R;
 
 public class MenuValidasiWajah extends AppCompatActivity {
 
+    private final String TAG = MenuValidasiWajah.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_validasi_wajah);
+
+        Button callAddSetWajah = (Button) findViewById(R.id.btn_add_set_wajah);
+        Button callDetectionView = (Button) findViewById(R.id.btn_detection_view);
+
+        callAddSetWajah.setOnClickListener(operate);
+        callDetectionView.setOnClickListener(operate);
     }
+
+    View.OnClickListener operate = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.btn_add_set_wajah:
+                    Intent intentToSetWajah = new Intent(v.getContext(), AddSetWajahActivity.class);
+                    intentToSetWajah.putExtra("identitas_mahasiswa", "5113100112 Ronald Gunawan R");
+                    startActivity(intentToSetWajah);
+                    break;
+                case R.id.btn_detection_view:
+                    Intent intentToDetectionView = new Intent(v.getContext(), DetectionActivity.class);
+                    startActivity(intentToDetectionView);
+                    break;
+            }
+        }
+    };
 }
--- app/src/main/java/id/web/goronald/facerecognitionsikemastc/activity/PilihMetodeValidasi.java
+++ app/src/main/java/id/web/goronald/facerecognitionsikemastc/activity/PilihMetodeValidasi.java
@@ -1,15 +1,44 @@
 package id.web.goronald.facerecognitionsikemastc.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import id.web.goronald.facerecognitionsikemastc.R;
 
-public class PilihValidasiMenuActiviy extends AppCompatActivity {
+public class PilihMetodeValidasi extends AppCompatActivity {
+
+    private final String TAG = PilihMetodeValidasi.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pilih_validasi_menu_activiy);
+        setContentView(R.layout.activity_pilih_metode_validasi);
+
+        Button btnValidasiTtd = (Button) findViewById(R.id.btn_validasi_ttd);
+        Button btnValidasiWajah = (Button) findViewById(R.id.btn_validasi_wajah);
+
+        btnValidasiTtd.setOnClickListener(validate);
+        btnValidasiWajah.setOnClickListener(validate);
     }
+
+    View.OnClickListener validate = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.btn_validasi_ttd:
+                    Intent intentToValidateSignature = new Intent(PilihMetodeValidasi.this, ValidasiTtd.class);
+                    startActivity(intentToValidateSignature);
+                    break;
+                case R.id.btn_validasi_wajah:
+                    Intent intentToValidateFace = new Intent(PilihMetodeValidasi.this, MenuValidasiWajah.class);
+                    startActivity(intentToValidateFace);
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
 }
