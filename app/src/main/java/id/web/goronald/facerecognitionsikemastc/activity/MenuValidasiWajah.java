da21ede7aad1dcd4f1cd83e5e1c0889a165f3cdf
SE\5113100112 <geronald10@gmail.com>
2017-03-15 16:39:03 +0700

    Add Detection Activity

---
+++ app/src/main/java/id/web/goronald/facerecognitionsikemastc/activity/MenuValidasiWajah.java
@@ -0,0 +1,15 @@
+package id.web.goronald.facerecognitionsikemastc.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import id.web.goronald.facerecognitionsikemastc.R;
+
+public class MenuValidasiWajah extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_validasi_wajah);
+    }
+}
